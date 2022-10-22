dcb5015fd2011762ea234bcea23abb9a9d0aa852
Jose Antonio Torralba <jatorralba.dev@gmail.com>
2022-10-22 16:03:30 +0200

    Estructura inicial generada con start.spring.io

---
+++ src/main/java/com/onlinestore/stockmangement/StockMangementApplication.java
@@ -0,0 +1,13 @@
+package com.onlinestore.stockmangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMangementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlinestore/stockmangement/StockMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinestore.stockmangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
